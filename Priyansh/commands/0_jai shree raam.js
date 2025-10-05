4f15a17ef56669e11dec89acb86901acdcac59f8
FYTERR575 <nadankhan661969@gmail.com>
2025-10-05 23:25:40 +0500

    Update 0_Naaina bot

--- Priyansh/commands/0_jai shree raam.js
+++ Priyansh/commands/0_jai shree raam.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "Shri ram",
+  name: "NAaina Bot",
     version: "1.1.1",
   hasPermssion: 0,
-  credits: "ABHISHEK", 
+  credits: "Naaina", 
   description: "Just Respond",
   commandCategory: "no prefix",
     cooldowns: 5, 
@@ -12,15 +12,15 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
-  if(react.includes("jay shri ram") ||
-     react.includes("jay siya ram") || react.includes("JAY SHRI RAM") || react.includes("jay mahakal") ||
-react.includes("ram ram") ||
-react.includes("Jay shri ram")) {
+  if(react.includes("Naina here") ||
+     react.includes("massom bachi") || react.includes("JAY SHRI RAM") || react.includes("jay mahakal") ||
+react.includes("Most wanted criminal naaina") ||
+react.includes("Naaina bolti public")) {
     var msg = {
-        body: `【  जय श्री राम ________________❤️🙏  】`,
+        body: `【 Naina bot ________________❤️🙏  】`,
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🙏", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🥰", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
