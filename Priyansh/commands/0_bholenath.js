b99c4fbbab3c0998a7b73854ccd47a1c76f8be41
FYTERR575 <nadankhan661969@gmail.com>
2025-10-06 00:29:05 +0500

    Update 0_bholenath.js

--- Priyansh/commands/0_bholenath.js
+++ Priyansh/commands/0_bholenath.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "mhadev",
+  name: "Naina",
     version: "1.1.1",
   hasPermssion: 0,
-  credits: "ABHISHEK", 
+  credits: "NAAINA", 
   description: "Just Respond",
   commandCategory: "no prefix",
     cooldowns: 5, 
@@ -12,15 +12,15 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
-  if(react.includes("har har mhadev") ||
-     react.includes("HAR HAR MHADEV") || react.includes("Har har mhadev") || react.includes("jai bholenath") ||
+  if(react.includes("bolo darling") ||
+     react.includes("bolk darling") || react.includes("bolo darling") || react.includes("jai bholenath") ||
 react.includes("jai shiv sambhoo") ||
 react.includes("Har har mhadev")) {
     var msg = {
-        body: `【  𝐇𝐀𝐑 𝐇𝐀𝐑 𝐌𝐇𝐀𝐃𝐄𝐕______🙏🏻🙏🏻🙏🏻❤️】`,
+        body: `【  𝗡𝗔𝗔𝗜𝗡𝗔 𝗖𝗔𝗥𝗧𝗢𝗢𝗡 ______❤️】`,
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🙏🏻", event.messageID, (err) => {}, true)
+    api.setMessageReaction("♥️", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
